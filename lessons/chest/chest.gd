9e07276c9901a00b5091e484f37f98ff67e06c58
avchowgda <avchow@s.sfusd.edu>
2026-02-03 11:50:02 -0800

    M6L5 finished

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,6 +1,19 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var is_open := false
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
 
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
@@ -11,9 +24,18 @@ func _ready() -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
 
 func _on_mouse_entered() -> void:
+	if is_open:
+		return
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness,3.0,6.0,0.08)
 
 func _on_mouse_exited() -> void:
+	if is_open:
+		return
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness,6.0,3.0,0.08)
+
+func open() -> void:
+	animation_player.play("open")
+
+	input_pickable = false
